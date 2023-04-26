b3fc7a4e2cc8fb5d1a309194495bd4f3347f5e18
nylus1 <ivb.ignacio@gmail.com>
2023-04-27 00:59:01 +0200

    Formateo de código en general

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // Animación barra de navegación
 
 function navBar() {
-    var nav = document.querySelector('nav');
-    window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
-            nav.classList.add('bg-dark', 'shadow');
-        } else {
-            nav.classList.remove('bg-dark');
-            nav.classList.add('transition-025')
-        }
-    })
+  var nav = document.querySelector("nav");
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+      nav.classList.add("bg-dark", "shadow");
+    } else {
+      nav.classList.remove("bg-dark");
+      nav.classList.add("transition-025");
+    }
+  });
 }
 
 // Hotfix para que en formato móvil se muestren bien los titulos
@@ -28,77 +28,81 @@ function navBar() {
 //     window.addEventListener('resize', addClassToMobileHeadings);
 // }
 
-
 // Animación para que aparezcan de primeras suave
 
 function popItems() {
+  const popElements = document.querySelectorAll(".pop");
 
-    const popElements = document.querySelectorAll(".pop");
+  popElements.forEach((element) => {
+    element.style.opacity = 0;
+  });
 
-    popElements.forEach(element => {
-        element.style.opacity = 0;
+  function showElementSmoothly(element) {
+    let opacity = 0;
+    const intervalId = setInterval(function () {
+      opacity += 0.05;
+      element.style.opacity = opacity;
+      if (opacity >= 1) {
+        clearInterval(intervalId);
+      }
+    }, 20);
+  }
+
+  setTimeout(function () {
+    popElements.forEach((element) => {
+      showElementSmoothly(element);
     });
-
-    function showElementSmoothly(element) {
-        let opacity = 0;
-        const intervalId = setInterval(function () {
-            opacity += 0.05;
-            element.style.opacity = opacity;
-            if (opacity >= 1) {
-                clearInterval(intervalId);
-            }
-        }, 20);
-    }
-
-    setTimeout(function () {
-        popElements.forEach(element => {
-            showElementSmoothly(element);
-        });
-    }, 250);
+  }, 250);
 }
 
 //Boton volver arriba de la página
 
 function backToTop() {
-    if (window.scrollY != 0) {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
+  if (window.scrollY != 0) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 }
 
-window.addEventListener('scroll', function () {
-    const backToTopBtn = document.getElementById('backToTopBtn');
-    if (window.scrollY > 400) {
-        backToTopBtn.style.display = 'block';
-    } else {
-        backToTopBtn.style.display = 'none';
-    }
+window.addEventListener("scroll", function () {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+  if (window.scrollY > 400) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
 });
 
-document.getElementById('backToTopBtn').addEventListener('click', function () {
-    backToTop();
+document.getElementById("backToTopBtn").addEventListener("click", function () {
+  backToTop();
 });
 
 //Animaciones objetos, añadiendo la clase indicada
 
 function animateOnScroll() {
-    const elements = document.querySelectorAll('.fade-in, .slide-in-left, .slide-in-right, .zoom-in, .rotate-in, .animate-from-bottom');
+  const elements = document.querySelectorAll(
+    ".fade-in, .slide-in-left, .slide-in-right, .zoom-in, .rotate-in, .animate-from-bottom"
+  );
 
-    elements.forEach(element => {
-      const elementTop = element.getBoundingClientRect().top;
-      const elementBottom = element.getBoundingClientRect().bottom;
+  elements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const elementBottom = element.getBoundingClientRect().bottom;
 
-      if (elementTop < window.innerHeight && elementBottom > 0) {
-        const animations = ['fade-in', 'slide-in-left', 'slide-in-right', 'zoom-in', 'rotate-in', 'animate-from-bottom'];
-        const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+    if (elementTop < window.innerHeight && elementBottom > 0) {
+      const animations = [
+        "fade-in",
+        "slide-in-left",
+        "slide-in-right",
+        "zoom-in",
+        "rotate-in",
+        "animate-from-bottom",
+      ];
+      const randomAnimation =
+        animations[Math.floor(Math.random() * animations.length)];
 
-        element.classList.add('active', randomAnimation);
-      }
-    });
-  }
+      element.classList.add("active", randomAnimation);
+    }
+  });
+}
 
-  window.addEventListener('load', animateOnScroll);
-  window.addEventListener('scroll', animateOnScroll);
-
-
-
-
+window.addEventListener("load", animateOnScroll);
+window.addEventListener("scroll", animateOnScroll);
